cb20917828c84878fb83e5d2c6a7432cb579eb92
Kseniya <baharevaxen@yandex.ru>
2025-10-08 15:15:29 +0300

    create admin.register

--- new_app/admin.py
+++ new_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Category
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'photo', 'category', 'price', 'created_at', 'updated_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+    search_fields = ('name',)
+
+
+
+
