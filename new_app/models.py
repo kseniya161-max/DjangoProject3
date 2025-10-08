3a01d4abe65e818647eb211a79838c96052abede
Kseniya <baharevaxen@yandex.ru>
2025-10-08 14:09:16 +0300

    correcting models

--- new_app/models.py
+++ new_app/models.py
@@ -1,21 +1,5 @@
 from django.db import models
 
-class Product(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Наименование")
-    description = models.CharField(max_length=200, verbose_name="Описание")
-    photo = models.ImageField(verbose_name="Изображение")
-    category = models.CharField(max_length=200, verbose_name="Категория")
-    price = models.IntegerField(verbose_name="Цена за единицу товара")
-    created_at = models.DateField(verbose_name="Дата создание")
-    updated_at = models.DateField(verbose_name="Дата изменения")
-
-    def __str__(self):
-        return f'{self.name} {self.price}'
-
-    class Meta:
-        verbose_name = 'Наименование'
-        verbose_name_plural = 'Наименование'
-        ordering = ['name']
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Наименование")
@@ -28,3 +12,22 @@ class Category(models.Model):
         verbose_name = 'Наименование'
         verbose_name_plural = 'Наименования'
         ordering = ['name']
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.CharField(max_length=200, verbose_name="Описание")
+    photo = models.ImageField(upload_to='products/', verbose_name="Изображение")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена за единицу товара")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создание")
+    updated_at = models.DateTimeField(auto_now=True,verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f'{self.name} {self.price}'
+
+    class Meta:
+        verbose_name = 'Наименование'
+        verbose_name_plural = 'Наименование'
+        ordering = ['name']
+
+
