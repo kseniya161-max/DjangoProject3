d2bf2ac51cc31c816dde0ac102fd7def14d951cf
Kseniya <baharevaxen@yandex.ru>
2025-10-18 18:23:43 +0300

    new controller

--- travel_app/urls.py
+++ travel_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from travel_app. apps import NewAppConfig
-from travel_app. views import home
+from travel_app.views import home, product_detail
 
 app_name = "travel_app"
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('travel_product/<int: product_id>', product_detail, name='product_detail')
+
 ]
