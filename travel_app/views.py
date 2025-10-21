0fb85fb91fb098021ead4fd965cf25e77cdb6335
Kseniya <baharevaxen@yandex.ru>
2025-10-21 16:15:35 +0300

    correcting views

--- travel_app/views.py
+++ travel_app/views.py
@@ -1,9 +1,5 @@
-from string import Template
-
-from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, TemplateView, CreateView
-
 from .models import Product, ProductForm
 from django.core.paginator import Paginator
 
@@ -19,15 +15,6 @@ class HomeListView(ListView):
         return Product.objects.all()
 
 
-# def home(request):
-#     """Отображает главную страницу приложения"""
-#     products = Product.objects.all()
-#     paginator = Paginator(products, 6)
-#     page_number = request.GET.get('page')
-#     page_obj = paginator.get_page(page_number)
-#     return render(request, 'home.html', {'page_obj': page_obj})
-
-
 class ProductDetailView(DetailView):
     """Отображает страницу продукта"""
     model = Product
@@ -35,37 +22,16 @@ class ProductDetailView(DetailView):
     context_object_name= 'travel_product'
 
 
-#
-# def product_detail(request, product_id):
-#     """Отображает страницу продукта"""
-#     travel_product = get_object_or_404(Product, id=product_id)
-#     return render(request, 'product_detail.html', {'travel_product': travel_product })
-
-
 class ContactView(TemplateView):
     """Отображает страницу с контактами"""
     template_name= 'contact.html'
 
-# def contact(request):
-#     return render(request, 'contact.html', {})
-#
+
 class AddProductView(CreateView):
     model = Product
     form_class = ProductForm
     template_name = 'add_product.html'
     success_url = reverse_lazy('travel_app:home')
-
-
-# def add_product(request):
-#     if request.method == 'POST':
-#         form = ProductForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('travel_app:home')
-#     else:
-#         form = ProductForm()
-#     return render(request, 'add_product.html', {'form':form})
-
 
 
 
