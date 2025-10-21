cf4d64a4b744b80a20b953c6c13cb27a33b58ad7
Kseniya <baharevaxen@yandex.ru>
2025-10-21 19:03:05 +0300

    correcting urls

--- blog/urls.py
+++ blog/urls.py
@@ -7,8 +7,8 @@ from blog.views import BlogPostCreateView, BlogPostDetailView, HomeView
 app_name = "blog"
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('blog/<int:pk>/', BlogPostDetailView.as_view(), name='blog_detail'),
+    path('', HomeView.as_view(), name='home_blog'),
+    path('post/<int:pk>/', BlogPostDetailView.as_view(), name='blog_detail'),
     path('form/', BlogPostCreateView.as_view(), name='add_post'),
 
 ]
--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('travel_app.urls', namespace='travel_app')),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls', namespace='blog')),
   ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
