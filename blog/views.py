c35dcef8c6af759b0b86c4e853f1809ce33c35f1
Kseniya <baharevaxen@yandex.ru>
2025-10-21 17:25:56 +0300

    creating CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
-# Create your views here.
+from blog.models import BlogPost
+
+
+class HomeView(ListView):
+    """Представление главной страницы"""
+    model = 'BlogPost'
+    template_name = 'home_blog.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return BlogPost.objects.all()
+
+
+class BlogPostDetailView(DetailView):
+    """ Отображает записи каждого отдельного блога"""
+    model = 'BlogPost'
+    template_name = 'detail_blog.html'
+    context_object_name = 'post' # В шаблоне ссылаемся на него
+
+
+class BlogPostCreateView(CreateView):
+    """Создает запись Блога"""
+    model = 'BlogPost'
+    template_name = 'form_blog.html'
+    fields = ['header', 'content', 'preview', 'created_at','is_published']
+    success_url = reverse_lazy('blog:post_list')
+
+
+class BlogUpdateView(UpdateView):
+    """Редактирует запись Блога"""
+    model = 'BlogPost'
+    template_name = 'form_blog.html'
+    fields = ['header', 'content', 'preview', 'created_at','is_published']
+    success_url = reverse_lazy('blog:post_list')
+
+
+class BlogDeleteView(UpdateView):
+    """Удаляет запись Блога"""
+    model = 'BlogPost'
+    template_name = 'blog_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
+
