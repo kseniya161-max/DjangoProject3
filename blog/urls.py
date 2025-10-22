6e084f803999de34a68b53f3be49af7fac9a986b
Kseniya <baharevaxen@yandex.ru>
2025-10-22 12:05:18 +0300

    correcting urls and viws get_queryset

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.admin import views
 from django.urls import path, include
 
-from blog.views import BlogPostCreateView, BlogPostDetailView, HomeView
+from blog.views import BlogPostCreateView, BlogPostDetailView, HomeView, BlogUpdateView,BlogDeleteView
 
 
 app_name = "blog"
@@ -9,6 +9,7 @@ app_name = "blog"
 urlpatterns = [
     path('', HomeView.as_view(), name='home_blog'),
     path('post/<int:pk>/', BlogPostDetailView.as_view(), name='blog_detail'),
+    path('post/<int:pk>/edit/', BlogUpdateView.as_view(), name='post_update'),
     path('form/', BlogPostCreateView.as_view(), name='add_post'),
-
+    path('post/<int:pk>/delete/', BlogDeleteView.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.template.defaultfilters import default
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -11,7 +12,7 @@ class HomeView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        return BlogPost.objects.all()
+        return BlogPost.objects.filter(is_published=True)
 
 
 class BlogPostDetailView(DetailView):
@@ -20,13 +21,19 @@ class BlogPostDetailView(DetailView):
     template_name = 'detail_blog.html'
     context_object_name = 'post' # В шаблоне ссылаемся на него
 
+    def get_object(self, queryset = None):
+        post = super().get_object(queryset)
+        post.views_count += 1
+        post.save()
+        return post
+
 
 class BlogPostCreateView(CreateView):
     """Создает запись Блога"""
     model = BlogPost
     template_name = 'form_blog.html'
     fields = ['header', 'content', 'preview','is_published']
-    success_url = reverse_lazy('blog:home')
+    success_url = reverse_lazy('blog:home_blog')
 
 
 class BlogUpdateView(UpdateView):
@@ -34,13 +41,16 @@ class BlogUpdateView(UpdateView):
     model = BlogPost
     template_name = 'form_blog.html'
     fields = ['header', 'content', 'preview', 'is_published']
-    success_url = reverse_lazy('blog:home')
+
+
+    def get_success_url(self):
+        return reverse_lazy('blog:blog_detail', kwargs={'pk': self.object.pk})
 
 
 class BlogDeleteView(DeleteView):
     """Удаляет запись Блога"""
     model = BlogPost
     template_name = 'blog_confirm_delete.html'
-    success_url = reverse_lazy('blog:home')
+    success_url = reverse_lazy('blog:home_blog')
 
 
