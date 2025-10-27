7695d5e522578cd8fe4bf49b14681ac3b4763a69
Kseniya <baharevaxen@yandex.ru>
2025-10-27 14:49:13 +0300

    new commit and new branch

--- travel_app/forms.py
+++ travel_app/forms.py
@@ -1,9 +1,9 @@
 from django.forms import ModelForm
-
 from travel_app.models import Product
 
 
 class ProductForm(ModelForm):
+    """ Форма Продукта"""
     class Meta:
         model = Product
         fields = '__all__'
