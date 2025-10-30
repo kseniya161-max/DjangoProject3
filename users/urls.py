05a606ff1c33715788ed98e94693ebe49ce2d261
Kseniya <baharevaxen@yandex.ru>
2025-10-30 14:01:54 +0300

    correcting url CreteUserView

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 from users.apps import UsersConfig
 from django.urls import path, include
+from users. views import CreateUserView
 
 
 app_name = "users"
@@ -9,5 +10,6 @@ app_name = "users"
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('register/', CreateUserView.as_view(), name='register'),
 
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import UserRegisterForm
+from users.models import User
+
+
+class CreateUserView(CreateView):
+    """Создаем представление для регистрации"""
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'login.html'
+    success_url = reverse_lazy('users:login')
