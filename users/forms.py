6b31d1d06ca249812ebf921e8c8718f1f21fbce8
Kseniya <baharevaxen@yandex.ru>
2025-10-30 18:44:00 +0300

    correcting forms

--- users/forms.py
+++ users/forms.py
@@ -9,3 +9,21 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+
+
+        self.fields['email'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите email'
+        })
+        self.fields['password1'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите пароль'
+        })
+
+        self.fields['password2'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Введите еще раз пароль'
+        })
