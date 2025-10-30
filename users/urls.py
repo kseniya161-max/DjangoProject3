cc1b8a097c79c70ab7a852b0a255aa55e5d45606
Kseniya <baharevaxen@yandex.ru>
2025-10-30 18:24:26 +0300

    not redaction  for anonim

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from users.apps import UsersConfig
 from django.urls import path, include
-from users. views import CreateUserView,email_verification
+from users. views import CreateUserView
 
 
 app_name = "users"
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', CreateUserView.as_view(), name='register'),
-    path('email-confirm/<str:token>/', email_verification, name='email-confirm'),
-
 ]
+
