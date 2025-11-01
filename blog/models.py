a8698488798d29c222cd32c74d4acb3524647847
Kseniya <baharevaxen@yandex.ru>
2025-11-01 19:07:39 +0300

    correcting models blog

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 class BlogPost(models.Model):
     header = models.CharField(max_length = 200 )
@@ -7,12 +8,13 @@ class BlogPost(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_published = models.BooleanField(default=False)
     views_count = models.PositiveIntegerField(default=0)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Владелец поста')
 
     class Meta:
         ordering = ['-created_at']
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
-        permissiond = [
+        permissions = [
             ('can_publish_blog_post', 'Can publish blog post'),
             ('can_unpublish_blog_post', 'Can unpublish blog post'),
             ('can_delete_blog_post', 'Can delete blog post'),
