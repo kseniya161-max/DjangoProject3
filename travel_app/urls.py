22b34bf72082aa0084cb295bb4bd2b68c0014b38
Kseniya <baharevaxen@yandex.ru>
2025-11-11 12:14:58 +0300

    create path to category(url)

--- travel_app/urls.py
+++ travel_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.admin import views
 from django.urls import path, include
 from travel_app. apps import NewAppConfig
-from travel_app.views import HomeListView, ProductDetailView, ContactView,AddProductView, ProductUpdateView, ProductDeleteView
+from travel_app.views import HomeListView, ProductDetailView, ContactView,AddProductView, ProductUpdateView, ProductDeleteView, CategoryProductListView
 from django.views.decorators.cache import cache_page
 
 app_name = "travel_app"
@@ -13,5 +13,6 @@ urlpatterns = [
     path('add_product/', AddProductView.as_view(), name='add_product'),
     path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
+    path('category/', CategoryProductListView.as_view(), name='category'),
 
 ]
