a07e597ea8a46890eee89f0592468f7497858497
Kseniya <baharevaxen@yandex.ru>
2025-11-12 17:50:49 +0300

    correcting settings 'TIMEOUT'

--- config/settings.py
+++ config/settings.py
@@ -167,6 +167,7 @@ if CACHE_ENABLED:
         'default': {
             'BACKEND': 'django.core.cache.backends.redis.RedisCache',
             'LOCATION': 'redis://127.0.0.1:6379/1',
+            'TIMEOUT': 300,
         }
     }
 
